e2834039e987c50cd118318d209a184e653d00af
anthony <floresgomezanthony@gmail.com>
2024-01-17 21:16:58 -0400

    set env vars

--- main.go
+++ main.go
@@ -1,50 +1,77 @@
 package main
 
 import (
-  "fmt"
-  "strings"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"time"
+
 	"github.com/gocolly/colly/v2"
 )
 
 var targets []string = []string{
-  "2010990022392",
-  "20386975",
-  "203.9",
+	"2010990022392",
+	"20386975",
+	"203.9",
 }
 
-func main(){
-	c := colly.NewCollector()
+var tgToken, tgTokenIsSet = os.LookupEnv("TG_TOKEN")
+var tgChannelName, tgChannelNameIsSet = os.LookupEnv("TG_CHANNEL_NAME")
 
+func main() {
+  if !tgTokenIsSet || !tgChannelNameIsSet {
+    slog.Error("TG env vars not set")
+    os.Exit(1)
+  }
+  
+  slog.Info(fmt.Sprintf("Scrapping started at %s", time.Now()))
+  // sendMessageToTgChannel(
+  //   tgToken, 
+  //   tgChannelName, 
+  //   fmt.Sprintf("Scrapping started at: %s", time.Now()),
+  // )
+
+	c := colly.NewCollector()
 	c.OnHTML("p", func(element *colly.HTMLElement) {
 		// fmt.Printf("%+v\n", e)
-    text := strings.ToLower(element.Text)
-    if targetMatchs(text) {
-      sendTgMessage(text)
-    }
+		text := strings.ToLower(element.Text)
+		if targetMatchs(text) {
+      slog.Info(fmt.Sprintf("Found: \n %s", text))
+      sendMessageToTgChannel(tgToken, tgChannelName, text)
+		} 
 	})
 
-	c.Visit("https://thor.organojudicial.gob.bo/")
+  if err := c.Visit("https://thor.organojudicial.gob.bo/"); err != nil {
+    errorMessage := fmt.Sprintf("Error scrapping. \n %s", err.Error())
+    slog.Error(errorMessage)
+  }
+
+  slog.Info("Scrapping finished")
 }
 
 func targetMatchs(text string) bool {
-  for _, targuet := range targets {
-    if strings.Contains(text, targuet) {
-      return true
-    }
-  }
+	for _, targuet := range targets {
+		if strings.Contains(text, targuet) {
+			return true
+		}
+	}
 
-  return false
+	return false
 }
 
-func sendMessageToTgChannel(tgBotToken string, tgChannelName string, message string) error {
-  encodedMsg := url.QueryEscape(message)
-  url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s", tgBotToken, tgChannelName, encodedMsg)
-  err := HttpGet(url)
-  if err != nil {
-    return err
-  }
-
-  return nil
+func sendMessageToTgChannel(tgBotToken string, tgChannelName string, message string) {
+	encodedMsg := url.QueryEscape(message)
+	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s", tgBotToken, tgChannelName, encodedMsg)
+	err := HttpGet(url)
+	if err != nil {
+    slog.Error(fmt.Sprintf("Error sending message to TG channel. \n %s", err.Error()))
+	}
 }
 
 func HttpGet(url string) error {
@@ -54,7 +81,7 @@ func HttpGet(url string) error {
 	}
 
 	if !(res.StatusCode == http.StatusOK || res.StatusCode == http.StatusAccepted) {
-		resBody, err := ioutil.ReadAll(res.Body)
+		resBody, err := io.ReadAll(io.Reader(res.Body))
 		if err != nil {
 			return errors.New(fmt.Sprintf("Unsuccessful %d http response. Cannot read response body.\n", res.StatusCode))
 		}
